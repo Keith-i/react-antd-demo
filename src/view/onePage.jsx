71a08bc801085cadb7eb273e593005ee0adccc71
Keith <754781293@qq.com>
2020-05-07 18:43:28 +0800

    提交

--- src/view/onePage.jsx
+++ src/view/onePage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Pagination } from 'antd'
+import { Button, Pagination, Rate } from 'antd'
 
 class OnePage extends Component {
   render() {
@@ -10,6 +10,7 @@ class OnePage extends Component {
         <Button type="dashed">Dashed</Button>
         <Button type="danger">Danger</Button>
         <Button type="link">Link</Button>
+        <Rate></Rate>
         <Pagination defaultCurrent={1} total={50}></Pagination>
       </div>
     )
