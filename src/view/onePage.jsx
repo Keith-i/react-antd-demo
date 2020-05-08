fd61639a8194e8b5176aa6ea667bee149cb61a23
Keith <754781293@qq.com>
2020-05-08 10:49:33 +0800

    提交

--- src/view/onePage.jsx
+++ src/view/onePage.jsx
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import { Button, Pagination, Rate } from 'antd'
 
 class OnePage extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      nums: 32
+    }
+  }
+
+  pageChange(page, pageSize) {
+    console.log(page, pageSize, this)
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +23,7 @@ class OnePage extends Component {
         <Button type="danger">Danger</Button>
         <Button type="link">Link</Button>
         <Rate></Rate>
-        <Pagination defaultCurrent={1} total={50}></Pagination>
+        <Pagination defaultCurrent={1} total={500} onChange={ this.pageChange }></Pagination>
       </div>
     )
   }
